889d0672b26f2b5950cfe939eaf11d3003206833
mufaqar <mufaqar@gmail.com>
2023-09-28 14:24:25 +0500

    update

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -12,6 +12,7 @@ const Main = () => {
                <Container className='flex flex-col justify-center text-white h-full z-10 relative items-center'>
                     <h6 className='uppercase font-medium text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center'>Home & Garden Masters</h6>
                     <p className='max-w-[900px] mt-4 text-center md:text-lg font-light'>Elevating Your Living Spaces to New Heights</p>
+                    <p className='max-w-[900px] mt-4 text-center md:text-lg font-light'>At Home&Garden Masters, we are committed to enhancing your property's beauty and functionality. With a wealth of experience and a passion for excellence, we invite you to embark on a journey of transformation with us.</p>
                     <Link href="/get-a-quote" className={`border p-1.5 px-8 rounded-md mt-10 font-light bg-white/20`}>Get a Free Qoute </Link>
                </Container>
           </main>
