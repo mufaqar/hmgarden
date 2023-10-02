23239e77c7fdc350aea1871feaaf840202a6021e
syed zunair gillani <zunairgillani54@gmail.com>
2023-10-02 18:41:03 +0500

    feture image of service slug

--- src/pages/services/[slug].jsx
+++ src/pages/services/[slug].jsx
@@ -3,14 +3,16 @@ import { Container, Header, MainHeading } from '@/components/imports'
 import { useRouter } from 'next/router'
 import {  ServiceSingle } from '../../config/query'
 import apolloClient from '../../config/client'
+import Image from 'next/image'
 
 const Slug = ({ singleService }) => {
 
      return (
           <>
                <Header black />
-               <Container className="pt-20 flex flex-col items-center">
+               <Container className="pt-20 flex flex-col  items-center">
                     <MainHeading>{singleService?.title}</MainHeading>
+                    <Image src={singleService?.featuredImage?.node?.mediaItemUrl} className=" max-w-[980px] w-full mt-10" alt="image" width={800} height={720}/>
                    <div className='mt-4 text-center max-w-[980px] single_service'>
                    <div
                    dangerouslySetInnerHTML={{ __html: singleService.content }}
