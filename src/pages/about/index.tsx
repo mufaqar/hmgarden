c2c0a9bacf4d352f93a077c77671be2d17a3f8e2
mufaqar <mufaqar@gmail.com>
2023-10-10 14:42:55 +0500

    about

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -7,24 +7,24 @@ const About = () => {
      return (
           <>
                <Header black />
-               <Container className="py-20 flex flex-col items-center">
+               <Container className="py-20 flex flex-col items-center max-w-[960px]">
                     <MainHeading>Why Choose Us?</MainHeading>
-                    <p className='mt-4 text-center max-w-[800px]'>Competitive and Reliable Pricing: Home&Garden Masters offers pricing that not only fits your budget but also provides excellent value for your investment. Our transparent and competitive rates ensure that you receive the best services without breaking the bank.</p>
+                    <p className='mt-4 text-center '><strong>Competitive and Reliable Pricing:</strong> Home&Garden Masters offers pricing that not only fits your budget but also provides excellent value for your investment. Our transparent and competitive rates ensure that you receive the best services without breaking the bank.</p>
 
-                    <p className='mt-4 text-center max-w-[800px]'>6 Years of Proven Experience: With six years of dedicated experience, we have a proven track record of delivering high-quality results. Our extensive experience enables us to tackle a wide range of projects with confidence and expertise.</p>
+                    <p className='mt-4 text-center '><strong>6 Years of Proven Experience: </strong>With six years of dedicated experience, we have a proven track record of delivering high-quality results. Our extensive experience enables us to tackle a wide range of projects with confidence and expertise.</p>
 
-                    <p className='mt-4 text-center max-w-[800px]'>
-                         Fully Trained and Professional Teams: Our teams are comprised of highly skilled and trained professionals who are passionate about what they do. They bring expertise, creativity, and professionalism to every project, ensuring your satisfaction.
-                    </p><p className='mt-4 text-center max-w-[800px]'>
-                         Comprehensive Insurance Coverage: Home&Garden Masters takes the safety of your property seriously. We are fully insured, providing you with peace of mind knowing that your investment and property are protected throughout the duration of our services.
-                    </p><p className='mt-4 text-center max-w-[800px]'>
-                         Commitment to Sustainability: We prioritize eco-friendly practices and materials, contributing to a greener and more sustainable environment. Our dedication to sustainability reflects our responsibility towards the community and the planet.
-                    </p><p className='mt-4 text-center max-w-[800px]'>
-                         Customer-Centric Approach: Your needs and preferences are our top priority. We collaborate closely with you, ensuring that our services are tailored to meet your specific requirements and expectations.
-                    </p><p className='mt-4 text-center max-w-[800px]'>
-                         Exceptional Workmanship: Our commitment to quality is unwavering. We take pride in delivering exceptional workmanship in every project, using top-tier materials and attention to detail to ensure lasting results.
-                    </p><p className='mt-4 text-center max-w-[800px]'>
-                         Comprehensive Service Offerings: Whether it's transforming your outdoor spaces, enhancing your interiors, or maintaining your property, Home&Garden Masters offers a comprehensive range of services, simplifying your home and garden improvement needs under one roof
+                    <p className='mt-4 text-center '>
+                    <strong> Fully Trained and Professional Teams:</strong> Our teams are comprised of highly skilled and trained professionals who are passionate about what they do. They bring expertise, creativity, and professionalism to every project, ensuring your satisfaction.
+                    </p><p className='mt-4 text-center '>
+                    <strong>  Comprehensive Insurance Coverage:</strong> Home&Garden Masters takes the safety of your property seriously. We are fully insured, providing you with peace of mind knowing that your investment and property are protected throughout the duration of our services.
+                    </p><p className='mt-4 text-center '>
+                    <strong>  Commitment to Sustainability:</strong> We prioritize eco-friendly practices and materials, contributing to a greener and more sustainable environment. Our dedication to sustainability reflects our responsibility towards the community and the planet.
+                    </p><p className='mt-4 text-center '>
+                    <strong>  Customer-Centric Approach:</strong> Your needs and preferences are our top priority. We collaborate closely with you, ensuring that our services are tailored to meet your specific requirements and expectations.
+                    </p><p className='mt-4 text-center '>
+                    <strong>   Exceptional Workmanship: </strong>Our commitment to quality is unwavering. We take pride in delivering exceptional workmanship in every project, using top-tier materials and attention to detail to ensure lasting results.
+                    </p><p className='mt-4 text-center '>
+                    <strong>  Comprehensive Service Offerings:</strong> Whether it's transforming your outdoor spaces, enhancing your interiors, or maintaining your property, Home&Garden Masters offers a comprehensive range of services, simplifying your home and garden improvement needs under one roof
                     </p>
 
                </Container>
