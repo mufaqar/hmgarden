8675f51e8ddb91c295903e76c467056c3aebc6f4
mufaqar <mufaqar@gmail.com>
2023-10-10 14:38:05 +0500

    abut

--- src/components/aboutUs/AboutUs.tsx
+++ src/components/aboutUs/AboutUs.tsx
@@ -7,17 +7,17 @@ const AboutUs = () => {
           <section className='bg-black text-white py-12 pb-16'>
                <Container>
                     <MainHeading>About us</MainHeading>
-                    <p className='text-2xl text-center'> Home&Garden Masters: Transforming properties with 6 Years of Expertise</p>
+                    <p className='text-2xl text-center py-5'> Home&Garden Masters: Transforming properties with 6 Years of Expertise</p>
+                    <p className='text-xl leading-8 text-center py-5'>Home&Garden Masters, situated in the charming town of Grays Essex, brings six years of dedicated experience to the forefront of the home and garden improvement industry. We are your go-to specialists in landscaping, gardening, fencing, waste removal, tiling, painting, and pressure washing services.</p>
+                             
                     <div className="grid md:grid-cols-2 gap-8 mt-10">
                          <div className='text-center px-10'>
-                              <p>Home&Garden Masters, situated in the charming town of Grays Essex, brings six years of dedicated experience to the forefront of the home and garden improvement industry. We are your go-to specialists in landscaping, gardening, fencing, waste removal, tiling, painting, and pressure washing services.</p>
                               <figure className='relative mt-10'>
                                    <Image src="/images/about1.png" alt={`image`} width={590} height={664} className='z-10 shadow relative' />
                                    <div className=' absolute -top-5 -left-5 h-full w-full border border-white' />
                               </figure>
                          </div>
                          <div className='px-10'>
-                         <p className="text-center">Over the past six years, Home&Garden Masters has been on a mission to reshape and rejuvenate properties across Grays and its surrounding areas. Our journey began with a clear vision: to create and maintain exceptional outdoor and indoor spaces that captivate and inspire.</p>
                               <figure className='relative mt-10'>
                                    <Image src="/images/about2.png" alt={`image`} width={590} height={664} className='z-10 shadow relative' />
                                    <div className='w-full absolute -top-5 -left-5 h-full border border-white' />
--- src/components/aboutUs/ourMission.tsx
+++ src/components/aboutUs/ourMission.tsx
@@ -4,8 +4,8 @@ import { Container, MainHeading } from '../imports'
 const OurMission = () => {
   return (
     <Container className="flex flex-col items-center my-20">
-          <MainHeading>Our mission</MainHeading>
-          <p className='mt-4 text-center max-w-[900px]'>Fully Trained and Professional Teams: Our teams are comprised of highly skilled and trained professionals who are passionate about what they do. They bring expertise, creativity, and professionalism to every project, ensuring your satisfaction.</p>
+          <MainHeading>Our Journey:</MainHeading>
+          <p className='mt-4 text-center max-w-[900px]'>Over the past six years, Home&Garden Masters has been on a mission to reshape and rejuvenate properties across Grays and its surrounding areas. Our journey began with a clear vision: to create and maintain exceptional outdoor and indoor spaces that captivate and inspire.</p>
           <div className='grid grid-cols-4 w-full mt-12'>
                <div className=' flex justify-center flex-col items-center'>
                     <button className='text-white p-2 rounded-full bg-[#24AA02] text-xl md:text-3xl md:w-[65px] z-[1] md:h-[65px] w-[50px] h-[50px]'>01</button>
