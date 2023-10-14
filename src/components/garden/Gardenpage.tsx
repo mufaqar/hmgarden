378ae8f0158e28dbbc814ce7cd84d86bd90fcb9f
mufaqar <mufaqar@gmail.com>
2023-10-14 20:08:40 +0500

    format

--- src/components/garden/Gardenpage.tsx
+++ src/components/garden/Gardenpage.tsx
@@ -6,17 +6,11 @@ function Gardenpage() {
   return (
     <>
       <main className='mt-32 w-full h-screen bg-no-repeat bg-cover' style={{ backgroundImage: `url(${mainImage.src})` }}>
-        
-
-
       </main>
-     
       <Container className='mt-16'>
-               <MainHeading className="white">Meet the Home and Garden Masters Team</MainHeading>
-              
+        <MainHeading className="white">Meet the Home and Garden Masters Team</MainHeading>
         <p className='font-[500] mt-10'>Home and Garden Masters Team is a dedicated group of experts specializing in home and garden improvement. With a wealth of experience in landscaping, interior design, renovation, and outdoor maintenance, we're here to help you transform your living spaces into your dream environment. Our mission is to make your house a home and your garden a sanctuary.</p>
-
-          </Container>
+      </Container>
     </>
   )
 }
