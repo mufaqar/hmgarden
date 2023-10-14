5c18295402d9ad027b1596ab2275ffedb173e4f9
mufaqar <mufaqar@gmail.com>
2023-10-14 20:04:49 +0500

    upadted

--- src/components/garden/Gardenpage.tsx
+++ src/components/garden/Gardenpage.tsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import mainImage from '../../../public/images/main.jpg'
+import { Container, MainHeading } from '../imports'
 
 function Gardenpage() {
   return (
-    <main className=' mt-32 w-full min-h-[450px] bg-no-repeat bg-cover ' style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${mainImage.src})` }}>
-      <div className='container mx-auto px-4'>
-      <div className='lg:w-[1024px] w-full mx-auto text-center text-white pt-40'>
-         <h2 className='text-3xl lg:text-36px font-semibold'>Meet the Home and Garden Masters Team</h2>
+    <>
+      <main className='mt-32 w-full h-screen bg-no-repeat bg-cover' style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${mainImage.src})` }}>
+        
 
-         <p className='font-[500] mt-10'>Home and Garden Masters Team is a dedicated group of experts specializing in home and garden improvement. With a wealth of experience in landscaping, interior design, renovation, and outdoor maintenance, we're here to help you transform your living spaces into your dream environment. Our mission is to make your house a home and your garden a sanctuary.</p>
-          
-      </div>
-      
 
-    
+      </main>
+     
+      <Container className='mt-16'>
+               <MainHeading className="white">Meet the Home and Garden Masters Team</MainHeading>
+              
+        <p className='font-[500] mt-10'>Home and Garden Masters Team is a dedicated group of experts specializing in home and garden improvement. With a wealth of experience in landscaping, interior design, renovation, and outdoor maintenance, we're here to help you transform your living spaces into your dream environment. Our mission is to make your house a home and your garden a sanctuary.</p>
 
-      </div>
-    </main>
+          </Container>
+    </>
   )
 }
 
